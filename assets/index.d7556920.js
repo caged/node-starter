f4607578cae48050029237f68d8c4b5ec2d2d8d0
caged <justin@labratrevenge.com>
2021-06-03 04:49:56 +0000

    Deploying to gh-pages from @ caged/node-starter@d855340431ee885561f2c3b239a3b282990db857 🚀

---
+++ assets/index.d7556920.js
@@ -0,0 +1,1 @@
+document.querySelector("#app").innerHTML='\n  <h1>Great Job!</h1>\n  <a href="https://www.youtube.com/channel/UCsZSBJ83B_xTW-L_eVgm2Xw" target="_blank">Your website is ready.</a>\n';
